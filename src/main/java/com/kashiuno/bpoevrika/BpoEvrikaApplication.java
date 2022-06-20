35ae28ef5f72ccb94098799eec2b7575ce3e76dd
Denis_Krupin <denis.krupin@tvh.com>
2022-06-20 12:51:32 +0300

    Initial commit with compleated task!

---
+++ src/main/java/com/kashiuno/bpoevrika/BpoEvrikaApplication.java
@@ -0,0 +1,67 @@
+package com.kashiuno.bpoevrika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@SpringBootApplication
+@Controller
+public class BpoEvrikaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BpoEvrikaApplication.class, args);
+    }
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("exist")
+    public String exist(Model model, @RequestParam String user) throws IOException {
+        Process p = Runtime.getRuntime().exec("wmic UserAccount get Name");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        boolean exist = false;
+        for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+            if (line.isBlank()) continue;
+            if (line.trim().equals(user.trim())) exist = true;
+        }
+        model.addAttribute("user", user);
+        model.addAttribute("exist", exist);
+        return "exist";
+    }
+
+    @EventListener({ApplicationReadyEvent.class})
+    public void applicationReadyEvent() {
+        browse("http://localhost:8080");
+    }
+
+    public static void browse(String url) {
+        if(Desktop.isDesktopSupported()){
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                desktop.browse(new URI(url));
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        }else{
+            Runtime runtime = Runtime.getRuntime();
+            try {
+                runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/test/java/com/kashiuno/bpoevrika/BpoEvrikaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kashiuno.bpoevrika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpoEvrikaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
